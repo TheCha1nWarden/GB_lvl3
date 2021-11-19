07bfcfd59adce4357f2d48b0dcc30f992d7a8b90
Artem <artem.krugman14@gmail.com>
2021-11-19 22:50:53 +0300

    add homework №2

---
+++ src/ChatStarter1.java
@@ -0,0 +1,7 @@
+import adapter.ChatAdapter;
+
+public class ChatStarter1 {
+    public static void main(String[] args) {
+        new ChatAdapter();
+    }
+}
---
+++ src/adapter/ChatAdapter.java
@@ -0,0 +1,26 @@
+package adapter;
+
+import gui.ChatFrame;
+
+public class ChatAdapter {
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8189;
+
+    private final ChatFrame frame;
+    private final ChatConnector connector;
+
+    public ChatAdapter() {
+        this.connector = new ChatConnector(HOST, PORT);
+        this.frame = new ChatFrame(outboundMessage -> connector.sendMessage(outboundMessage));
+
+        while (true) {
+            try {
+                frame.onReceive().accept(connector.receiveMessage());
+            } catch (RuntimeException e) {
+                System.out.println("Клиент вышел из чата. Соединение разорвано");
+                System.exit(0);
+                break;
+            }
+        }
+    }
+}
